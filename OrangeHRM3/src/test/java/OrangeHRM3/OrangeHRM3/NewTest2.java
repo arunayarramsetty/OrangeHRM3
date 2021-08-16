312072ba4467293b6b97db6906f41840939c213e
arunayarramsetty <asred@LAPTOP-9HART8E0>
2021-08-16 11:55:38 +0530

    sadf

--- OrangeHRM3/src/test/java/OrangeHRM3/OrangeHRM3/NewTest2.java
+++ OrangeHRM3/src/test/java/OrangeHRM3/OrangeHRM3/NewTest2.java
@@ -2,10 +2,8 @@ package OrangeHRM3.OrangeHRM3;
 
 import org.testng.annotations.Test;
 
-public class NewTestTest {
+public class NewTest2 {
   @Test
-  public void Login_Test() throws Exception
-  
-  {
+  public void f() {
   }
 }
